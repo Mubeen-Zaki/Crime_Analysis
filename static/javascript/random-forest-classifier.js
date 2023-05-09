780f3a3ee99c84a283db557f8a3c6b0bdddb4a51
Mubeen-Zaki <mubeenzaki31@gmail.com>
2023-05-10 01:55:28 +0530

    chngs2

--- static/javascript/random-forest-classifier.js
+++ static/javascript/random-forest-classifier.js
@@ -6,30 +6,33 @@ function ChangeLabel() {
 
     const startIndex = text.indexOf('[') + 2;
     const endIndex = text.indexOf(']') - 1;
-    const zone = text.substring(0, startIndex);
+    const zone = text.substring(0, startIndex-2);
     let arr = text.substring(startIndex, endIndex + 1);
     arr = arr.slice(6);
     const array = arr.split(",");
-    const table = document.createElement("table");
+    var tableBody = '<table class="table shadow-soft rounded">'
+    tableBody += '<tr>'
+        tableBody += '<th>STATE</th>';
+        tableBody += '<th>DISTRICT</th>';
+        tableBody += '<th>YEAR</th>';
+        tableBody += '<th>MURDER</th>';
+        tableBody += '<th>ATTEMPT TO MURDER</th>';
+        tableBody += '<th>RAPE</th>';
+        tableBody += '<th>KIDNAPPING & ABDUCTION</th>';
+        tableBody += '<th>DACOITY</th>';
+        tableBody += '<th>ROBBERY</th>';
+        tableBody += '<th>THEFT</th>';
+        tableBody += '<th>HURT</th>'
 
-    const table = document.createElement("table");
-
-    arr.forEach((item) => {
-        const tr = document.createElement("tr");
-        const td = document.createElement("td");
-        td.innerText = item;
-        tr.appendChild(td);
-        table.appendChild(tr);
-    });
-
-    array.forEach((item) => {
-        const tr = document.createElement("tr");
-        const td = document.createElement("td");
-        td.innerText = item;
-        tr.appendChild(td);
-        table.appendChild(tr);
-    });
-
+        tableBody += '</tr>';
+        tableBody += '<tr>';
+        array.forEach( function(ele)
+        {
+            tableBody += '<td class="border-0" scope="col">' + ele+'</td>';
+        });
+        tableBody += '</tr>';
+    text1.innerHTML='<b>'+zone+'</b>'+"     "+tableBody;
+    console.log(table);
 
 
 
